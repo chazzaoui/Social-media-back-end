fd05e8dd2a3909ef5e9818f437db509a6bc1a903
Charaf Azzaoui <charafazzaoui@gmail.com>
2020-05-06 19:50:12 +0200

    .gitignore fix

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,4 +1,4 @@
-const {validationResult} = require('express-validator/check')
+const {validationResult} = require('express-validator')
 
 exports.getPost = (req, res, next) => {
     res.status(200).json({posts: [{
