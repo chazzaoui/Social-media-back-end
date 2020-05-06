85142085d3c822b2393a2b325df80fbca725014e
Charaf Azzaoui <charafazzaoui@gmail.com>
2020-05-06 23:43:15 +0200

    (feat): add error handling

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const mongoose = require('mongoose');
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -9,6 +10,7 @@ require ('custom-env').env('staging')
 const feedRoutes = require('./routes/feed');
 
 app.use(bodyParser.json()); //this will make incoming data be parsed to json .
+app.use('./images', express.static(path.join(__dirname, 'images')));
 
 app.use((req, res, next) => {  // here i want to add headers so i can allow requests from different servers to be allowed in the app
 res.setHeader('Access-Control-Allow-Origin', '*'); // here we allow specific origins to allow our data, the * makes evreyone able to acces it
@@ -17,6 +19,13 @@ res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization'); //
 next();
 })
 app.use('/feed', feedRoutes);
+
+app.use((error, req, res, next) => {
+    console.log(error)
+    const status = error.statusCode || 500;
+    const message = error.message; //can be set globally in script by setting error.message
+    res.status(status).json({message})
+})
 
 mongoose.connect(process.env.DB_HOST, { useNewUrlParser: true, useUnifiedTopology: true }  )
 .then(() => {
--- controllers/feed.js
+++ controllers/feed.js
@@ -10,7 +10,9 @@ exports.getPost = (req, res, next) => {
 exports.addPost = (req, res, next) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()){ //means we have errors
-        return res.status(422).json({message: 'Entered data does not meet min length requirement', errors: errors.array()})
+        const error = new Error('Entered data does not meet min length requirement')
+        error.statusCode = 422;
+        throw error;
     }
     const title = req.body.title;
     const content = req.body.content
@@ -25,5 +27,11 @@ exports.addPost = (req, res, next) => {
     post.save().then(res =>{
          console.log(res)
          res.status(201).json({ message: "post uploaded successfully!", post: res })
-    }).catch(err => console.log(err))
+    })
+    .catch(err => { 
+        if (!err.statusCode){
+            err.statusCode = 500
+        } 
+        next(err);
+    })
 }
