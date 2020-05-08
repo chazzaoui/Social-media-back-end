7d9373cfa9d5b3563caebb3540a4a02144723e68
Charaf Azzaoui <charafazzaoui@gmail.com>
2020-05-08 18:42:24 +0200

    (feat): add routes to all middleware

--- routes/feed.js
+++ routes/feed.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const { body } = require("express-validator");
-const isAuth = require('../middleware/is-auth');
+const isAuth = require("../middleware/is-auth");
 
 const router = express.Router();
 
@@ -9,6 +9,7 @@ const feedController = require("../controllers/feed");
 router.get("/posts", isAuth, feedController.getPosts);
 router.post(
   "/posts",
+  isAuth,
   [
     body("title").trim().isLength({ min: 5 }),
     body("content").trim().isLength({ min: 5 }),
@@ -16,10 +17,11 @@ router.post(
   feedController.addPost
 );
 
-router.get("/post/:postId", feedController.getPost);
+router.get("/post/:postId", isAuth, feedController.getPost);
 
 router.put(
   "/post/:postId",
+  isAuth,
   [
     body("title").trim().isLength({ min: 5 }),
     body("content").trim().isLength({ min: 5 }),
@@ -27,6 +29,6 @@ router.put(
   feedController.editPost
 );
 
-router.delete("/post/:postId", feedController.deletePost);
+router.delete("/post/:postId", isAuth, feedController.deletePost);
 
 module.exports = router;
